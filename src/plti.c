e5af71ec6c2c7c0031ea5653bb7f8af7c60c396a
Joe <vpnlocationus@gmail.com>
2026-05-26 07:29:32 +0300

    Fix: prevent memory corruption using MAP_FIXED_NOREPLACE
    - Replace MAP_FIXED with MAP_FIXED_NOREPLACE in plti_internal_set_got_entry().
    - This prevents unintentional overwriting of existing memory mappings if the
      hint address is already occupied.
    - Added a fallback mechanism to allocate memory safely without a hint

--- src/plti.c
+++ src/plti.c
@@ -259,7 +259,9 @@ static bool plti_internal_set_got_entry(struct elf_info *info, uintptr_t got_add
   if (!stash) {
     /* INFO: Don't call LOGE -- or else it will try to access GOT while it's being modified */
     void *hint = find_high_backup_hint(vma_len);
-    void *backup_addr = mmap(hint, vma_len, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    void *backup_addr = MAP_FAILED;
+    if (hint)
+      backup_addr = mmap(hint, vma_len, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED_NOREPLACE, -1, 0);
     if (backup_addr == MAP_FAILED) {
       backup_addr = mmap(NULL, vma_len, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
       if (backup_addr == MAP_FAILED) {
